38c329530f7e22aa7e5d61fc3ea49c26a478d854
Riley Kuhlman <rileyk42@icloud.com>
2018-02-19 16:07:59 -0600

    started working

--- Page.js
+++ Page.js
@@ -1,12 +1,46 @@
 var copiedText = "";
+var text1;
+var text2;
+var text3;
+var text4;
+var text5;
 $(function() {
+  chrome.storage.sync.get(['Text1', 'Text2', 'Text3', 'Text4', 'Text5'], function(items) {
+    if (!items['Text1']) {
+      chrome.storage.sync.set({
+        'Text1': "1"
+      });
+    }
+    if (!items['Text2']) {
+      chrome.storage.sync.set({
+        'Text2': "2"
+      });
+    }
+    if (!items['Text3']) {
+      chrome.storage.sync.set({
+        'Text3': "3"
+      });
+    }
+    if (!items['Text4']) {
+      chrome.storage.sync.set({
+        'Text4': "4"
+      });
+    }
+    if (!items['Text5']) {
+      chrome.storage.sync.set({
+        'Text5': "5"
+      });
+    }
+    text1 = items['Text1'];
+    text2 = items['Text2'];
+    text3 = items['Text3'];
+    text4 = items['Text4'];
+    text5 = items['Text5'];
+  });
   $("body").bind({
     copy: function() {
       copiedText = getSelectionText();
       saveChanges();
-    },
-    paste: function() {
-      console.log(window.clipboardData.getData('Text'));
     },
     cut: function() {
       copiedText = getSelectionText();
@@ -30,19 +64,27 @@ function saveChanges() {
     console.log('Error: No value specified');
     return;
   }
-  // Save it using the Chrome extension storage API.
-  chrome.storage.sync.set({
-    'Text': copiedText
-  }, function() {
-    // Notify that we saved.
-    console.log('Settings saved');
-  });
-  console.log(copiedText);
+  moveText(1);
+  updateSlotOne();
+  console.log('finished saving');
 }
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-  console.log("changed storage");
-  for (key in changes) {
-    var storageChange = changes[key];
-    console.log('Storage key "%s" in namespace "%s" changed. ' + 'Old value was "%s", new value is "%s".', key, namespace, storageChange.oldValue, storageChange.newValue);
-  }
-});
+
+function moveText(num) {
+  text5 = text4;
+  text4 = text3;
+  text3 = text2;
+  text2 = text1;
+  chrome.storage.sync.set({
+    'Text2': text2,
+    'Text3': text3,
+    'Text4': text4,
+    'Text5': text5
+  });
+}
+
+function updateSlotOne() {
+  text1 = copiedText;
+  chrome.storage.sync.set({
+    'Text1': copiedText,
+  });
+}
--- popup.js
+++ popup.js
@@ -1,13 +1,20 @@
 $(function() {
-  console.log("Add event listener for copy and paste");
-  chrome.storage.sync.get(['Text'], function(items) {
-    console.log(items.Text);
-  });
-  chrome.storage.onChanged.addListener(function(changes, namespace) {
-    console.log("changed storage");
-    for (key in changes) {
-      var storageChange = changes[key];
-      console.log('Storage key "%s" in namespace "%s" changed. ' + 'Old value was "%s", new value is "%s".', key, namespace, storageChange.oldValue, storageChange.newValue);
+  for (var i = 1; i < 6; i++) {
+    setSpot(i);
+  }
+});
+
+function scrollDown() {
+  $('html, body').animate({
+    scrollTop: $("#spot1").offset().top
+  }, 500);
+}
+
+function setSpot(num) {
+  chrome.storage.sync.get('Text' + num, function(item) {
+    $("#spot" + num).text(item["Text" + num]);
+    if (num == 5) {
+      scrollDown();
     }
   });
-});
+}
